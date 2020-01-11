922dc27ac20291b07d1187bf51062f7f9687249f
se-n-pu-u-ki <1023.suzuki.m@gmail.com>
2020-01-11 11:59:54 +0900

    first commit

---
+++ src/js/pages/Layout.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+
+class Layout extends React.Component {
+  navigate() {
+    console.log(this.props.history);
+    this.props.history.push("/");
+  }
+  render() {
+    return (
+      <div>
+        <h1>KillerNews.net</h1>
+        {this.props.children}
+        <Link to="archives"><button class="btn btn-danger">archives</button></Link>,
+        <Link to="settings"><button class="btn btn-success">settings</button></Link>
+        <button class="btn btn-info" onclick={this.navigate.bind(this)}>featured</button>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Layout);
